8894ec232cf58a1efda4fcf68382cb8623724a8d
Ioann <78559802+Ioann717@users.noreply.github.com>
2023-10-26 18:28:26 +0300

    Update e4.3.js

--- e4.3.js
+++ e4.3.js
@@ -1,57 +1,65 @@
 # Задание №1
 
-def print_own_properties(obj):
-    if isinstance(obj, dict):
-        for key, value in obj.items():
-            print(f"Key: {key}, Value: {value}")
-    else:
-        for key in dir(obj):
-            if not callable(getattr(obj, key)) and not key.startswith("__"):
-                value = getattr(obj, key)
-                print(f"Key: {key}, Value: {value}")
-
-// Используем:
-
-sample_object = {
-    "name": "Ioann",
-    "age": 27,
-    "city": "Krasnodar"
+function printOwnProperties(obj) {
+    if (typeof obj === "object" && !Array.isArray(obj) && obj !== null) {
+        if (typeof obj === "object") {
+            for (let key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    let value = obj[key];
+                    console.log("Key:", key, ", Value:", value);
+                }
+            }
+        }
+    } else {
+        for (let key in obj) {
+            if (typeof obj[key] !== "function" && !key.startsWith("__")) {
+                let value = obj[key];
+                console.log("Key:", key, ", Value:", value);
+            }
+        }
+    }
 }
 
-class SampleClass:
-    def __init__(self):
-        self.property1 = "Value 1"
-        self.property2 = "Value 2"
 
-sample_instance = SampleClass()
+let sampleObject = {
+    name: "Ioann",
+    age: 27,
+    city: "Krasnodar"
+};
 
-print("Dictionary:")
-print_own_properties(sample_object)
+class SampleClass {
+    constructor() {
+        this.property1 = "Value 1";
+        this.property2 = "Value 2";
+    }
+}
 
-print("Class instance:")
-print_own_properties(sample_instance)
+let sampleInstance = new SampleClass();
+
+console.log("Object properties:");
+printOwnProperties(sampleObject);
+
+console.log("Class instance properties:");
+printOwnProperties(sampleInstance);
+
 
 
 # Задание №2
 
-python
-def check_property(string, obj):
-    if hasattr(obj, string):
-        return True
-    else:
-        return False
+function checkProperty(string, obj) {
+  return obj.hasOwnProperty(string);
+}
 
+class MyClass {
+  constructor() {
+    this.my_property = "Hello";
+  }
+}
 
-// Используем:
+const obj = new MyClass();
+console.log(checkProperty("my_property", obj));  // Output: true
+console.log(checkProperty("other_property", obj));  // Output: false
 
-python
-class MyClass:
-    def __init__(self):
-        self.my_property = "Hello"
-
-obj = MyClass()
-print(check_property("my_property", obj))  # Output: True
-print(check_property("other_property", obj))  # Output: False
 
 
 # Задание №3
